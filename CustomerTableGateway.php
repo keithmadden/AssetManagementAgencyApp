de480ba8a3525c2a0bcecc7a9cb1a586b1fbe6d2
Keith Madden <keith-madden@hotmail.com>
2015-03-03 14:51:58 +0000

    Merge origin/master
    Conflicts:
    	BranchTableGateway.php
    	Connection.php
    	CustomerTableGateway.php
    	createCustomer.php
    	createCustomerForm.php
    	editCustomer.php
    	editCustomerForm.php
    	home.php
    	index.php
    	viewBranch.php
    	viewCustomer.php

--- CustomerTableGateway.php
+++ CustomerTableGateway.php
@@ -1,30 +1,22 @@
 <?php
-
 class CustomerTableGateway {
-
     private $connection;
-
     public function __construct($c) {
         $this->connection = $c;
     }
-
     public function getCustomers() {
         // execute a query to get all customer
         $sqlQuery = 
                 "SELECT c.*, b.address AS bankAddress
                 FROM customer c
                 LEFT JOIN branch b ON b.branch_id = c.branch_id";
-
         $statement = $this->connection->prepare($sqlQuery);
         $status = $statement->execute();
-
         if (!$status) {
             die("Could not retrieve customers");
         }
-
         return $statement;
     }
-
     public function getCustomerById($id) {
         // execute a query to get the user with the specified id
         $sqlQuery = 
@@ -32,26 +24,20 @@ class CustomerTableGateway {
                     FROM customer c
                     LEFT JOIN branch b ON b.branch_id = c.branch_id
                     WHERE c.`Customer ID` = :CustomerID";
-
         $statement = $this->connection->prepare($sqlQuery);
         $params = array(
             "CustomerID" => $id
         );
-
         $status = $statement->execute($params);
-
         if (!$status) {
             die("Could not retrieve user");
         }
-
         return $statement;
     }
-
     public function insertCustomer($n, $a, $m, $e, $bId) {
         $sqlQuery = "INSERT INTO customer " .
                 "(name, address, mobile, email, branch_id) " .
                 "VALUES (:name, :address, :mobile, :email, :branch_id)";
-
         $statement = $this->connection->prepare($sqlQuery);
         $params = array(
             "name" => $n,
@@ -60,35 +46,25 @@ class CustomerTableGateway {
             "email" => $e,
             "branch_id" => $bId
         );
-
         $status = $statement->execute($params);
-
         if (!$status) {
             die("Could not insert customer");
         }
-
         $id = $this->connection->lastInsertId();
-
         return $id;
     }
-
     public function deleteCustomer($id) {
         $sqlQuery = "DELETE FROM customer WHERE `Customer ID` = :CustomerID";
-
         $statement = $this->connection->prepare($sqlQuery);
         $params = array(
             "CustomerID" => $id
         );
-
         $status = $statement->execute($params);
-
         if (!$status) {
             die("Could not delete user");
         }
-
         return ($statement->rowCount() == 1);
     }
-
     public function updateCustomer($id, $n, $a, $m, $e, $bId) {
         $sqlQuery =
                 "UPDATE customer SET " .
@@ -98,104 +74,16 @@ class CustomerTableGateway {
                 "mobile = :mobile, " .
                 "email = :email " .
                 "WHERE `Customer ID` = :CustomerID";
-
         $statement = $this->connection->prepare($sqlQuery);
         $params = array(
             "CustomerID" => $id,
             "branch_id" => $bId,
-                "SELECT c.*, b.address AS bankAddress " .
-                "FROM customer c " .
-                "LEFT JOIN branch b ON b.branch_id = c.branch_id";
-
-        $statement = $this->connection->prepare($sqlQuery);
-        $status = $statement->execute();
-
-        if (!$status) {
-            die("Could not retrieve users");
-        }
-
-        return $statement;
-    }
-
-    public function getCustomerById($id) {
-        // execute a query to get the user with the specified id
-        $sqlQuery = "SELECT * FROM customer WHERE `Customer ID` = :CustomerID";
-
-        $statement = $this->connection->prepare($sqlQuery);
-        $params = array(
-            "CustomerID" => $id
-        );
-
-        $status = $statement->execute($params);
-
-        if (!$status) {
-            die("Could not retrieve user");
-        }
-
-        return $statement;
-    }
-
-    public function insertCustomer($n, $a, $m, $e) {
-        $sqlQuery = "INSERT INTO customer " .
-                "(name, address, mobile, email) " .
-                "VALUES (:name, :address, :mobile, :email)";
-
-        $statement = $this->connection->prepare($sqlQuery);
-        $params = array(
             "name" => $n,
             "address" => $a,
             "mobile" => $m,
             "email" => $e
         );
-
         $status = $statement->execute($params);
-
-        if (!$status) {
-            die("Could not insert user");
-        }
-
-        $id = $this->connection->lastInsertId();
-
-        return $id;
-    }
-
-    public function deleteCustomer($id) {
-        $sqlQuery = "DELETE FROM customer WHERE `Customer ID` = :CustomerID";
-
-        $statement = $this->connection->prepare($sqlQuery);
-        $params = array(
-            "CustomerID" => $id
-        );
-
-        $status = $statement->execute($params);
-
-        if (!$status) {
-            die("Could not delete user");
-        }
-
-        return ($statement->rowCount() == 1);
-    }
-
-    public function updateCustomer($id, $n, $a, $m, $e) {
-        $sqlQuery =
-                "UPDATE customer SET " .
-                "name = :name, " .
-                "address = :address, " . 
-                "mobile = :mobile, " .
-                "email = :email " .
-                "WHERE `Customer ID` = :CustomerID";
-
-        $statement = $this->connection->prepare($sqlQuery);
-        $params = array(
-            "CustomerID" => $id,
-            "name" => $n,
-            "address" => $a,
-            "mobile" => $m,
-            "email" => $e
-        );
-
-        $status = $statement->execute($params);
-
         return ($statement->rowCount() == 1);
     }
 }
